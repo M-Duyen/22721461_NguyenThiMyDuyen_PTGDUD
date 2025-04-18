bdfd6c948616d01a1b3f34a0716338ffbf759aeb
M-Duyen <22721461.duyen@student.iuh.edu.vn>
2025-04-19 02:04:09 +0700

    commit09

--- Lab_week_07/src/App.jsx
+++ Lab_week_07/src/App.jsx
@@ -4,6 +4,11 @@ import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
 import Sidebar from './component/Sidebar';
 import Header from './component/Header';
 import Dashboard from './component/Dashboard';
+import Projects from './pages/Projects';
+import Teams from './pages/Teams';
+import Analytics from './pages/Analytics';
+import Message from './pages/Messages';
+import Integrations from './pages/Integrations';
 
 function AppContent() {
     return (
@@ -16,7 +21,19 @@ function AppContent() {
                 <div className="grid grid-cols-2 h-20 pt-5.5 border-b border-b-gray-200">
                     <Header />
                 </div>
-                <Dashboard />
+               
+                    <Routes>
+                        <Route path="/dashboard" element={<Dashboard />} />
+                        <Route path="/projects" element={<Projects />} />
+                        <Route path="/teams" element={<Teams />} />
+                        <Route path="/analytics" element={<Analytics />} />
+                        <Route path="/messages" element={<Message />} />
+                        <Route
+                            path="/integrations"
+                            element={<Integrations />}
+                        />
+                    </Routes>
+                
             </div>
         </div>
     );
